a71081bb1d896a3d5d2bb9a807ae2bc443e137df
Shevchuk Volodymyr <ghgerzx007@gmail.com>
2026-07-19 14:41:58 +0200

    update tasks

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,11 +1,11 @@
-interface product {
+interface Product {
   readonly id: number;
   title: string;
   description?: string;
 }
 
-const product = {
-  id: 1,
+const product: Product = {
+   id: 1,
   title: "Tablet",
   description: "Compact and fast",
 };
